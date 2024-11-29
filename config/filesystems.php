7a7b8617ac197f0420ab5c6f796af48d950ce5a0
amjad7amoda <ahamoda161@gmail.com>
2024-11-29 11:58:32 +0300

    Delete Gallery Driver from filesystems.php

--- config/filesystems.php
+++ config/filesystems.php
@@ -45,13 +45,6 @@ return [
             'throw' => false,
         ],
 
-          'gallery' => [
-              'driver' => 'local',
-              'root' => storage_path('app/public/gallery'),
-              'url' => env('APP_URL'). '/storage',
-              'visibility' => 'public'
-          ],
-
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
