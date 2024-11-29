750d3152330a785979e4d80f82128edb572f66af
amjad7amoda <ahamoda161@gmail.com>
2024-11-29 19:19:55 +0300

    Full Product and Images System

--- app/Http/Controllers/Api/ImageController.php
+++ app/Http/Controllers/Api/ImageController.php
@@ -1,7 +1,9 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
+use App\Models\Product;
+use Illuminate\Routing\Controller;
 use App\Models\Image;
 use Illuminate\Http\Request;
 
@@ -10,25 +12,72 @@ class ImageController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(int $product)
     {
-        //
+        $product = Product::where('id', $product)->first();
+        if (!$product) {
+            return response()->json(['error' => 'This product is not exists']);
+        }
+
+        $images = $product->images->map(function ($image) {
+            $image->path = asset($image->path);
+            return $image; // إنشاء الرابط الكامل
+        });
+
+        return response()->json(['images' => $images]);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request, int $product)
     {
-        //
+        $product = Product::where('id', $product)->first();
+        if (!$product) {
+            return response()->json(['error' => 'This product is not exists']);
+        }
+
+        $validatedData = $request->validate([
+            'path'   => 'required|array',
+            'path.*' => 'file|mimes:jpg,jpeg,png'
+        ]);
+
+        $images = $request->file('path.*');
+        $imagesPath = [];
+        $folderName = "gallery/products/product-$product->id";
+        $imagesNumber = $product->images()->count();
+        foreach ($images as  $image) {
+            $imagesNumber++;
+            $imageName = "image-$product->id-". $imagesNumber . '.png';
+            $imagePath = $image->storeAs($folderName, $imageName, 'public');
+            $imagesPath[] = $imagePath;
+        }
+
+        foreach($imagesPath as $path){
+            $product->images()->create([
+                'path' => $path
+            ]);
+        }
+
+        return response()->json(['message' => 'Images has been added successfully']);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Image $image)
+    public function show(int $product, int $image)
     {
-        //
+        $product = Product::where('id', $product)->first();
+        if (!$product) {
+            return response()->json(['error' => 'This product is not exists']);
+        }
+        $image = Image::where('id', $image)->first();
+        if (!$image) {
+            return response()->json(['error' => 'This image is not exists']);
+        }
+
+        $image->path = asset('storage/'.$image->path);
+        return response()->json(['image' => $image]);
     }
 
     /**
--- database/migrations/2024_11_26_174058_create_products_table.php
+++ database/migrations/2024_11_26_174058_create_products_table.php
@@ -4,6 +4,7 @@ use App\Models\Store;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Storage;
 
 return new class extends Migration
 {
@@ -29,5 +30,10 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('products');
+
+
+        if(Storage::disk('public')->exists('gallery/products')){
+            Storage::disk('public')->deleteDirectory('gallery/products');
+        }
     }
 };
